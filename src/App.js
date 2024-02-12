ad12d83ed005bcfb86bd8119c956e53bf9119bee
KAMILA KARAMANOVA <tp051798@mail.apu.edu.my>
2024-02-12 20:28:49 +0800

    added the get request and error handling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import React, { useState } from "react";
+import {Octokit} from 'octokit';
+
 function App() {
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target.value);
+  };
+
+  const clickHandler = async (event) => {
+    try {
+      event.preventDefault();
+      const octokit = new Octokit({});
+    
+      const res = await octokit.request('GET /users/{username}/repos', {
+        username: searchInput,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+        })
+      console.log(res);
+    } catch(error){
+      alert(`User's repository is not found`);
+      setSearchInput("");
+    };
+  }
+
   return (
     <div>
       <header>
+        <h1>GitHub Explorer</h1>
       </header>
+      <form onSubmit={clickHandler}>
+        <input type="text"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}/>
+        <input type="submit"
+        value="Search"/>
+      </form>
+      
     </div>
   );
 }
