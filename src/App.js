f9dfcff3f1e2456d8c4d5034670667d04b6c51d4
KAMILA KARAMANOVA <tp051798@mail.apu.edu.my>
2024-09-05 17:02:57 +0800

    Fixed the class to className

--- src/App.js
+++ src/App.js
@@ -56,11 +56,11 @@ function App() {
               </div>
             </div> 
           </form>
-          <ul class="list-group list-group-flush">
+          <ul className="list-group list-group-flush">
             {searchResult.map((repo) => (
               <Link to={`/repository-info/${repo.owner.login}/${repo.name}`} 
                 key={repo.id}>
-                  <li class="list-group-item mt-2 mb-2">
+                  <li className="list-group-item mt-2 mb-2">
                     {repo.name}
                   </li>
               </Link>))
